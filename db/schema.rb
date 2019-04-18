7406300eefff1bb80c5f786441e95d39349ed613
Yousri <laifayousri@gmail.com>
2019-04-18 18:36:53 +0200

    Modification des table cities dogsitters et dogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190418155343) do
+ActiveRecord::Schema.define(version: 20190418162141) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "city_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_dogs_on_city_id"
   end
 
   create_table "dogsitters", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_dogsitters_on_city_id"
   end
 
   create_table "strolls", force: :cascade do |t|
