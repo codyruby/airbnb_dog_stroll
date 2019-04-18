c9c823d6d111d475207a19b7b66bc2080b890245
Yousri <laifayousri@gmail.com>
2019-04-18 18:58:42 +0200

    Modification du fichier seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# 15.times do 
+#     City.create(city_name: Faker::Nation.capital_city)
+# end
+
+# 20.times do
+#     Dogsitter.create!(name: Faker::Name.name, city_id: Faker::Number.between(1, 15))
+# end
+
+# 20.times do
+#     Dog.create!(name: Faker::Creature::Dog.name, city_id: Faker::Number.between(1, 15))
+# end
+
+# 40.times do
+#     Stroll.create!(dogsitter_id: Faker::Number.between(1, 20), dog_id: Faker::Number.between(1, 20), date: Faker::Date.between_except(1.year.ago, 1.year.from_now, Date.today))
+# end
